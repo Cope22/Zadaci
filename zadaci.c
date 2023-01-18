0e0a29f44a0d22d0c1ff0a182a0e1d1bbc33701d
Petar Jevtic <petarjevtic02@gmail.com>
2023-01-18 13:00:05 +0100

    first commit

---
+++ zadaci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main ()
+
+{
+
+int i;
+for (i=0;i<4;i++)
+
+switch (i) {
+
+case 1: putchar ('E'); break;
+case 3: putchar ('I');
+case 4: putchar ('V'); break;
+case 6: putchar ('N'); break;
+default: putchar ('T');
+
+}
+
+putchar ('A');
+
+
+}
+
+//Rezultat: Tetiva//
