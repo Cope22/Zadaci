aba1a3226fe7b2d391ecb7ba50c033d22a0fd95c
Petar Jevtic <petarjevtic02@gmail.com>
2023-01-18 14:58:01 +0100

    Treci zadatak

---
+++ Treci zadatak.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main ()
+{
+
+int a[] = {1,2,3,4,5},i;
+for (i=0;i<5;i++)
+a[i]+=1;
+
+}
+
+Rezultat: 1 3 5 7 9
+
+// Objasnjenje: i=o a[0] = 1+0=1, i=1 a[1] = 2+1=3, i=2 a[2] = 3+2=5, i=3 a[3] = 4+3=7, i=4 a[4] = 5+4=9//
+
